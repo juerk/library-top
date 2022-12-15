92e2338b3253c4e7d89c205ce7b4a37e5d037a18
juerk <wuerkerrs06@gmail.com>
2022-12-15 09:24:02 -0700

    Figured logic for render divs along with array addition/removal

--- script.js
+++ script.js
@@ -1,55 +1,48 @@
 let myLibrary = [];
+let book = "book";
 
-function Book(title, author, pages) {
-  this.title = title;
-  this.author = author;
-  this.pages = pages;
+// function Book(title, author, pages) {
+//   this.title = title
+//   this.author = author
+//   this.pages = pages
+// }
 
-  this.showTitle = function() {
-    return this.title
-  }
-  this.showAuthor= function() {
-    return this.author
-  }
-  this.showPages = function() {
-    return this.pages
-  }
-}
+// const newBook = new Book(title, author, pages) 
 
 function addBookToLibrary() {
-  let newBook = new Book (document.getElementById("title").value, document.getElementById("author").value, document.getElementById("pages").value);
-
-  let bookDiv = document.querySelector("#bookDiv");
-  let newDiv = document.createElement("div");
-  bookDiv.appendChild(newDiv)
-
-  let titleSpan = document.createElement("span")
-  titleSpan.textContent = newBook.showTitle()
-  newDiv.appendChild(titleSpan)
-
-  let authorSpan = document.createElement("span")
-  authorSpan.textContent = newBook.showAuthor()
-  newDiv.appendChild(authorSpan)
-
-  let pagesSpan = document.createElement("span")
-  pagesSpan.textContent = newBook.showPages()
-  newDiv.appendChild(pagesSpan)
-
-  myLibrary.push(newBook)
-  // myLibrary.push(newBook.showTitle(), newBook.showAuthor(), newBook.showPages())
-  console.log(myLibrary)
+  myLibrary.push(book)
+  render()
 }
 
-function popFunction() {
-  myLibrary.pop();
-  console.log(myLibrary)
-}
+function render() {
+  const display = document.getElementById('mainDiv');
+  const books = document.querySelectorAll('.bookDiv');
+  books.forEach(book => display.removeChild(book));
 
-function formToggle() {
-  let x = document.getElementById("formdiv");
-  if (x.style.display === "block") {
-    x.style.display = "none";
-  } else {
-    x.style.display = "block";
+  for (let i = 0; i < myLibrary.length; i++) {
+    createBook(myLibrary[i])
   }
 }
+
+function createBook(item) {
+  let mainDiv = document.getElementById('mainDiv')
+  let removeButton = document.createElement('button')
+  let bookDiv = document.createElement('div')
+
+  bookDiv.classList.add('bookDiv')
+  bookDiv.setAttribute('id', myLibrary.indexOf(item))
+
+  removeButton.setAttribute('id', 'removeBtn')
+  removeButton.innerText = "Remove"
+  bookDiv.appendChild(removeButton)
+  mainDiv.appendChild(bookDiv)
+
+  removeButton.addEventListener('click', () => {
+    myLibrary.splice(myLibrary.indexOf(item), 1);
+    render()
+  })
+  console.log(myLibrary)
+}
+
+
+
